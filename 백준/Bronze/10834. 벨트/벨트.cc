cc66a9d8b27fe002e48ec9514c045843b5011f60
jake1104 <81201317+jake1104@users.noreply.github.com>
2024-12-14 16:14:12 +0900

    [Bronze II] Title: 벨트, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.cc
+++ 백준/Bronze/10834. 벨트/벨트.cc
@@ -4,23 +4,13 @@
 int main() {
 	int M;
 	scanf("%d", &M);
-	int B[1000][2];
-	bool d = true;
-	int a = 1;
-	int b = 1;
+	int B[1000][3];
+	int d = 0;
+	int s = 1;
 	for (int i = 0; i < M; i++) {
 		scanf("%d %d %d", &B[i][0], &B[i][1], &B[i][2]);
-		if (B[i][2] == 1) {
-			d = !d;
-		}
-		a *= B[i][0];
-		b *= B[i][1];
+		d = (d + B[i][2]) % 2;
+		s = (s / B[i][0]) * B[i][1];
 	}
-	if (d) {
-		printf("0 ");
-	}
-	else {
-		printf("1 ");
-	}
-	printf("%d", (int)b / a);
+	printf("%d %d", d, s);
 }
