ce3c0ea23963a74b82572b05d6900ae0faa57479
jake1104 <81201317+jake1104@users.noreply.github.com>
2024-12-28 16:16:46 +0900

    [Silver I] Title: 볼 모으기, Time: 8 ms, Memory: 1480 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.cc
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.cc
@@ -8,25 +8,45 @@ int main() {
 	scanf("%s", bs);
 	bool rc = false;
 	bool bc = false;
-	int r = 0;
-	int b = 0;
-	int rr = 0;
-	int rb = 0;
-	for (int i = N - 1; i >= 0; i--) {
+	int r1 = 0;
+	int b1 = 0;
+	int r2 = 0;
+	int b2 = 0;
+	for (int i = 0; i < N; i++) {
 		if (bs[i] == 'R') {
 			rc = true;
 		}
 		if (rc) {
 			if (bs[i] == 'B')
-				r++;
+				r1++;
 		}
 		if (bs[i] == 'B') {
 			bc = true;
 		}
 		if (bc) {
 			if (bs[i] == 'R')
-				b++;
+				b1++;
 		}
 	}
+	rc = false;
+	bc = false;
+	for (int i = N - 1; i >= 0; i--) {
+		if (bs[i] == 'R') {
+			rc = true;
+		}
+		if (rc) {
+			if (bs[i] == 'B')
+				r2++;
+		}
+		if (bs[i] == 'B') {
+			bc = true;
+		}
+		if (bc) {
+			if (bs[i] == 'R')
+				b2++;
+		}
+	}
+	int b = b1 > b2 ? b2 : b1;
+	int r = r1 > r2 ? r2 : r1;
 	printf("%d", b > r ? r : b);
 }
