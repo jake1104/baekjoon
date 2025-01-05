9108e5be8621628c1d00fc39e6634bc92797cb7b
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-01-05 14:26:27 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -6,11 +6,11 @@ int main() {
 	int K;
 	scanf("%d %d", &N, &K);
 	int M[1001][4];
-	long S[1001];
+	long long S[1001];
 	int Ki;
 	for (int i = 0; i < N; i++) {
 		scanf("%d %d %d %d", &M[i][0], &M[i][1], &M[i][2], &M[i][3]);
-		S[i] = M[i][1] * 100000000 + M[i][2] * 10000 + M[i][3];
+		S[i] = M[i][1] * 100000000000 + M[i][2] * 1000000 + M[i][3];
 		if (M[i][0] == K) {
 			Ki = i;
 		}
