88cb7089d781366407b7ca6220e1ee94fc72fb72
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-01-12 16:42:47 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,17 +1,8 @@
 #include <stdio.h>
 int N, M;
 int arr[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-bool p(int n) {
-    bool c = true;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i != j && arr[i] == arr[j]) c = false;
-        }
-    }
-    return c;
-}
+bool in[8];
 void f(int n) {
-    if (!p(n)) return;
     if (n == M) {
         for (int i = 0; i < M; i++) {
             printf("%d ", arr[i]);
@@ -20,8 +11,13 @@ void f(int n) {
         return;
     }
     for (int i = 1; i <= N; i++) {
-        arr[n] = i;
-        f(n + 1);
+        if (!in[i])
+        {
+            in[i] = true;
+            arr[n] = i;
+            f(n + 1);
+            in[i] = false;
+        }
     }
 }
 int main() {
