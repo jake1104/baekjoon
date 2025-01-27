7a1942acc239166b806cb8804029366524ff184f
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-01-27 23:45:58 +0900

    [Bronze III] Title: 크림빵, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.cc
+++ 백준/Bronze/28214. 크림빵/크림빵.cc
@@ -3,7 +3,7 @@
 int main() {
     int N,K,P;
     scanf("%d %d %d",&N,&K,&P);
-    int t,c=0;
-    for(int i=0;i<N*K;i++){scanf("%d",&t);c+=t;}
-    printf("%d",c/(K-P+1)<N?c/(K-P+1):N);
+    int t,c,r=0;
+    for(int i=0;i<N;i++){c=0;for(int j=0;j<K;j++){scanf("%d",&t);c+=t;}if(c>=K-P+1){r++;}}
+    printf("%d",r);
 }
