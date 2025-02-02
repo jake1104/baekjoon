622f4b5cd9fcd23b4d3ddd6f2e97005450dd4de3
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-02-02 18:05:24 +0900

    [Bronze IV] Title: 주사위 세개, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,3 +1,3 @@
 L=list(map(int,input().split()))
 a,b,c=L[0],L[1],L[2]
-print(10000+a*1000 if a==b==c else  1000+a*100 if a==b or a==c else 1000+b*100 if b==c else max(L)*100)
+print(10000+a*1000 if a==b==c else 1000+a*100 if a==b or a==c else 1000+b*100 if b==c else max(L)*100)
