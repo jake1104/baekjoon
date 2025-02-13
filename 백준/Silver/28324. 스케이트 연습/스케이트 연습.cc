03ee3bb85c460c14c1314c547a1d21a86d402549
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-02-13 23:24:06 +0900

    [Silver IV] Title: 스케이트 연습, Time: 100 ms, Memory: 2948 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -2,11 +2,12 @@
 int main(){
     int N;scanf("%d",&N);
     int V[500001];V[N]=0;for(int i=0;i<N;i++)scanf("%d",&V[i]);
-    int S=0;
-    for(int i=0;i<=N;i++){
-        int M=500001;
-        for(int j=0;j<=N-i;j++)M=M<V[i+j]+j?M:V[i+j]+j;
-        S+=M;
+    long S=0,M,m=-1;
+    int i=N;int j=1;
+    while(i>=0){
+        j=j<V[i]?j:V[i];
+        S+=j;
+        i--;j++;
     }
-    printf("%d",S);
+    printf("%ld",S);
 }
