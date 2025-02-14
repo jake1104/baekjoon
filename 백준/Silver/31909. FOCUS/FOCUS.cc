7bb3be9293fdf83bd92e00f1820eed9903e7cdd1
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-02-14 17:14:24 +0900

    [Silver IV] Title: FOCUS, Time: 24 ms, Memory: 1112 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/31909. FOCUS/FOCUS.cc
+++ 백준/Silver/31909. FOCUS/FOCUS.cc
@@ -7,19 +7,22 @@ int main(){
         int a;scanf("%d",&a);
         int x=0,y=0;
         int c=0;
-        for(int i=0;i<=8;i++){
+        for(int i=0;i<8;i++){
             if(a%2==1){
-                if(c==0){x=i;c++;}
-                else if(c==1){y=i;c++;}
-                else{x=-1;y=-1;c++;}
+                if(c==0){
+                    x=i;c++;
+                }
+                else if(c==1){
+                    y=i;c++;
+                }
+                else{x=0;y=0;}
             }
             a/=2;
         }
-        if(!(x==-1&&y==-1)){
-            int t=k[x];
-            k[x]=k[y];
-            k[y]=t;
-        }
+        int t=k[x];
+        k[x]=k[y];
+        k[y]=t;
+        
     }
     int K;scanf("%d",&K);
     printf("%d",k[K]);
