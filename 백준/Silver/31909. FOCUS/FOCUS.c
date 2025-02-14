6243d5af87c7fcbd41611151a1bd9bcbf77f00c1
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-02-14 17:22:11 +0900

    [Silver IV] Title: FOCUS, Time: 24 ms, Memory: 1116 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/31909. FOCUS/FOCUS.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int N;scanf("%d",&N);
+    int k[8];
+    for(int i=0;i<8;i++)k[i]=i;
+    for(int i=0;i<N;i++){
+        int a;scanf("%d",&a);
+        int x=0,y=0;
+        int c=0;
+        for(int i=0;i<=8;i++){
+            if(a%2==1){
+                if(c==0){x=i;c++;}
+                else if(c==1){y=i;c++;}
+                else{x=-1;y=-1;c++;}
+            }
+            a/=2;
+        }
+        if(!(x==-1&&y==-1)){
+            int t=k[x];
+            k[x]=k[y];
+            k[y]=t;
+        }
+    }
+    int K;scanf("%d",&K);
+    printf("%d",k[K]);
+}
