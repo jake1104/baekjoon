65c42dbc1a0c2c454ba79c3272c1591a0378ab6d
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-02-17 14:30:56 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 60 ms, Memory: 1780 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+struct c{int x,y;};
+bool f(c a,c b){
+    if(a.y==b.y)return a.x<b.x;
+    return a.y<b.y;
+}
+int main(){
+    int N;scanf("%d",&N);
+    c I[100000];
+    for(int i=0;i<N;i++)scanf("%d %d",&I[i].x,&I[i].y);
+    sort(I,I+N,f);
+    for(int i=0;i<N;i++)printf("%d %d\n",I[i].x,I[i].y);
+}
