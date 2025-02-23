aff613f4696f5046f4f800634c518f243540583b
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-02-23 16:08:39 +0900

    [Gold V] Title: 가로등, Time: 108 ms, Memory: 21312 KB, Score: 40 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.cc
+++ 백준/Gold/32069. 가로등/가로등.cc
@@ -1,22 +1,31 @@
-#include <stdio.h>
-#include <algorithm>
+#include<stdio.h>
+#include<queue>
 using namespace std;
-int main() {
+typedef struct{long long p,d;} Node;
+int main(){
     long long L, N, K;
-    scanf("%lld %lld %lld",&L,&N,&K);
-    long long A[300000];
-    bool a[300000];
-    long long LK=L+1>K?L+1:K;
-    for(int i=0;i<LK;i++)a[i]=false;
-    for(int i=0;i<N;i++){scanf("%lld",&A[i]);a[A[i]]=true;}
-    long long S[300000];
-    for(int i=0;i<LK;i++){
-        S[i]=1000000000000000000;
-        for(int j=0;j<=(i>LK-i-1?i:LK-i-1);j++){
-            if(i-j>=0){if(a[i-j]){S[i]=j<S[i]?j:S[i];}}
-            if(i+j<LK){if(a[i+j]){S[i]=j<S[i]?j:S[i];}}
+    scanf("%lld %lld %lld", &L, &N, &K);
+    long long A[N];
+    for(long long i=0;i<N; i++) scanf("%lld", &A[i]);
+    bool V[L+1]={false};
+    queue<Node> q;
+    for(long long i=0;i<N;i++){
+        q.push((Node){A[i],0});
+        V[A[i]]=true;
+    }
+    long long r[K];
+    int c=0;
+    while(!q.empty()&&c<K){
+        Node n = q.front(); q.pop();
+        if(c<K)r[c++]=n.d;
+        if(n.p>0&&!V[n.p-1]){
+            V[n.p-1]=true;
+            q.push((Node){n.p-1,n.d+1});
+        }
+        if(n.p<L&&!V[n.p+1]){
+            V[n.p+1]=true;
+            q.push((Node){n.p+1,n.d+1});
         }
     }
-    sort(S,S+LK);
-    for(int i=0;i<K;i++)printf("%lld\n",S[i]);
+    for(int i=0;i<K;i++)printf("%lld\n", r[i]);
 }
