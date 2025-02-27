e846ca0c118756d4615ce82ed51761b186db8296
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-02-27 23:26:56 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 2008 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<vector>
+#include<queue>
+using namespace std;
+int main(){
+    int N;scanf("%d",&N);
+    priority_queue<int> pq;
+    for(int i=0;i<N;i++){
+        int x;scanf("%d",&x);
+        if(x==0){
+            if(pq.empty())printf("0\n");
+            else{
+                printf("%d\n",pq.top());
+                pq.pop();
+            }
+        }else pq.push(x);
+    }
+}
