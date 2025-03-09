9ab01e0360c35ff78b16174b38e19416ce54d8dd
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-03-09 15:26:52 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int N,M;
+int a[8];
+void f(int l,int d){
+    if(d==M){
+        for(int i=0;i<M;i++)printf("%d ",a[i]);
+        printf("\n");
+    }
+    for(int i=l+1;i<=N;i++){
+        a[d]=i;
+        f(i,d+1);
+    }
+}
+int main(){
+    scanf("%d %d",&N,&M);
+    f(0,0);
+}
