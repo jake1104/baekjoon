27d87232219b98b71a2c4cc0ce74b4d5cadab8cd
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-04-04 00:10:59 +0900

    [Gold II] Title: 누적 거리, Time: 128 ms, Memory: 8284 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/22345. 누적 거리/누적 거리.cc
+++ 백준/Gold/22345. 누적 거리/누적 거리.cc
@@ -1,15 +1,25 @@
-#include<stdio.h>
-#define ABS(x) (((x)<0)?-(x):(x))
-int N,Q;
-long long a[200000],x[200000];
-long long f(long long X){
-    long long s=0;
-    for(int i=0;i<N;i++)s+=a[i]*ABS(x[i]-X);
-    return s;
-}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
 int main(){
-    scanf("%d %d",&N,&Q);
-    for(int i=0;i<N;i++)scanf("%lld %lld",&a[i],&x[i]);
-    long long q[Q];for(int i=0;i<Q;i++)scanf("%lld",&q[i]);
-    for(int j=0;j<Q;j++)printf("%lld\n",f(q[j]));
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N,Q;cin>>N>>Q;
+    vector<pair<int,int>>v(N);
+    for(int i=0;i<N;++i)cin>>v[i].second>>v[i].first;
+    sort(v.begin(),v.end());
+    vector<int>x(N),a(N);
+    for(int i=0;i<N;++i)x[i]=v[i].first,a[i]=v[i].second;
+    vector<ll>p_a(N+1,0),p_ax(N+1,0);
+    for(int i=1;i<=N;++i)p_a[i]=p_a[i-1]+a[i-1],p_ax[i]=p_ax[i-1]+(ll)a[i-1]*x[i-1];
+    ll ta=p_a[N],tax=p_ax[N];
+    while(Q--){
+        int q;
+        cin>>q;
+        int idx=lower_bound(x.begin(),x.end(),q)-x.begin();
+        ll la=p_a[idx],lax=p_ax[idx],ra=ta-la,rax=tax-lax;
+        cout<<q*la-lax+(rax-q*ra)<<'\n';
+    }
 }
