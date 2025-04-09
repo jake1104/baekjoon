552e824b161a6484782985df3ba1d11b34be30c8
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-04-10 00:07:53 +0900

    [Gold IV] Title: 리조트, Time: 0 ms, Memory: 2060 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/13302. 리조트/리조트.cc
+++ 백준/Gold/13302. 리조트/리조트.cc
@@ -1,25 +1,20 @@
 #include<iostream>
 using namespace std;
-int N,C[102],r=0;
+int N,C[102],r=2147483647;
 int A[101][101];
 void f(int d,int c,int p){
+    while(C[d])d++;
+    if(p>=r)return;
     if(d>N){
-        if(!r)r=p;
-        else r=r<p?r:p;
+        r=r<p?r:p;
         return;
     }
-    if(!A[d][c])A[d][c]=p;
-    else if(A[d][c]>=p)A[d][c]=p;
+    if(!A[d][c]||A[d][c]>p)A[d][c]=p;
     else return;
-    if(C[d]){
-        while(C[d])d++;
-        f(d,c,p);
-        return;
-    }
     if(c>=3)f(d+1,c-3,p);
-    f(d+1,c,p+10000);
-    f(d+3,c+1,p+25000);
     f(d+5,c+2,p+37000);
+    f(d+3,c+1,p+25000);
+    f(d+1,c,p+10000);
     return;
 }
 int main(){
