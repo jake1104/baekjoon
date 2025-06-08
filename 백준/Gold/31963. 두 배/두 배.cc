589ecc335311d5148594dffc8d7cdbe5d0daa6d8
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-06-08 10:20:40 +0900

    [Gold III] Title: 두 배, Time: 84 ms, Memory: 2056 KB, Score: 33 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -2,12 +2,22 @@
 using namespace std;
 int main(){
     int N;cin>>N;
-    int l=0,c=0;
+    int l=0,c=0,lc=0,lo;
     while(N--){
         int t;cin>>t;
-        if(l==0)l=t;
+        if(l==0){l=t;lo=t;}
         else{
-            while(l>t){c++;t*=2;}
+            if(lo>=t){
+                lo=t;
+                t<<=lc;
+                while(l>t){t<<=1;lc++;}
+                c+=lc;
+            }else{
+                lc=0;
+                lo=t;
+                while(l>t){t<<=1;lc++;}
+                c+=lc;
+            }
             l=t;
         }
     }cout<<c;
