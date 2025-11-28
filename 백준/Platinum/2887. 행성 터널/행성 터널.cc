f108816ca613412cce0022f57ea486669317acad
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-11-29 01:20:09 +0900

    [Platinum V] Title: 행성 터널, Time: 84 ms, Memory: 7500 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.cc
+++ 백준/Platinum/2887. 행성 터널/행성 터널.cc
@@ -1,46 +1,53 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-struct node{int x,y,z,i;};
-struct edge{int A,B,C;};
-int abs(int x){return x<0?-x:x;}
-int g(node a,node b){
-    int t1=abs(a.x-b.x),t2=abs(a.y-b.y),t3=abs(a.z-b.z);
-    int t=t1<t2?t1:t2;
-    return t<t3?t:t3;
-}struct UF{
+struct Node{int x,y,z,i;};
+struct Edge{int a,b,c;};
+struct UF{
     vector<int>p;
     UF(int n):p(n){for(int i=0;i<n;i++)p[i]=i;}
-    int f(int x){return p[x]==x?x:p[x]=f(p[x]);}
-    bool u(int a,int b){
-        a=f(a);b=f(b);
+    int find(int x){
+        while(p[x]!=x){
+            p[x]=p[p[x]];
+            x=p[x];
+        }return x;
+    }bool uni(int a,int b){
+        a=find(a);b=find(b);
         if(a==b)return 0;
         p[b]=a;return 1;
     }
-};int main(){
+};inline int cost(const Node&a,const Node&b){
+    int t1=abs(a.x-b.x),t2=abs(a.y-b.y),t3=abs(a.z-b.z);
+    int t=t1<t2?t1:t2;
+    return t<t3?t:t3;
+}int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
     int N;cin>>N;
-    vector<node>I(N);
+    vector<Node>I(N);
     for(int i=0;i<N;i++){
         cin>>I[i].x>>I[i].y>>I[i].z;
         I[i].i=i;
-    }vector<edge>G;G.reserve(3*N);
+    }
+    vector<Edge>G;
+    G.reserve(3*N);
     auto add=[&](auto cmp){
         sort(I.begin(),I.end(),cmp);
         for(int i=0;i<N-1;i++){
-            node a=I[i], b=I[i+1];
-            G.push_back({a.i,b.i,g(a,b)});
+            const Node&a=I[i];
+            const Node&b=I[i+1];
+            G.push_back({a.i,b.i,cost(a,b)});
         }
     };
-    add([](node a,node b){return a.x<b.x;});
-    add([](node a,node b){return a.y<b.y;});
-    add([](node a,node b){return a.z<b.z;});
-    sort(G.begin(),G.end(),[](edge a,edge b){return a.C<b.C;});
-    UF uf(N);int m=0,cnt=0;
-    for(auto&e:G){
-        if(uf.u(e.A,e.B)){
-            m+=e.C;
+    add([](const Node&a,const Node&b){return a.x<b.x;});
+    add([](const Node&a,const Node&b){return a.y<b.y;});
+    add([](const Node&a,const Node&b){return a.z<b.z;});
+    sort(G.begin(),G.end(),[](const Edge&a,const Edge&b){return a.c<b.c;});
+    UF uf(N);
+    long long ans=0;
+    int cnt=0;
+    for(const auto&e:G){
+        if(uf.uni(e.a,e.b)){
+            ans+=e.c;
             if(++cnt==N-1)break;
         }
-    }cout<<m;
+    }cout<<ans;
 }
