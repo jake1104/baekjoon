4f94b03916ce450ed0fb87e2d369f47f03a23747
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-11-29 01:40:12 +0900

    [Diamond IV] Title: 이항 계수 6, Time: 60 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Diamond/14854. 이항 계수 6/이항 계수 6.cc
+++ 백준/Diamond/14854. 이항 계수 6/이항 계수 6.cc
@@ -1,129 +1,112 @@
 #include<iostream>
 #include<vector>
-#include<cmath>
 #include<map>
 using namespace std;
 typedef long long ll;
 const ll MOD=142857;
-ll M[4]={27,11,13,37},N[4]={11*13*37,27*13*37,27*11*37,27*11*13};
-map<ll,vector<vector<ll>>>precomputedC;
-map<ll,vector<ll>>preprodCache;
-ll gcd(ll a,ll b,ll&x,ll&y){ // Euclidean Algorithm
-    if(!b){
-        x=1;y=0;
-        return a;
-    }
+const ll M_list[4]={27,11,13,37};
+ll N_list[4];
+ll crt_inv[4];
+map<ll,vector<vector<ll>>>preC;
+vector<ll>preprod27;
+vector<ll>pow27_base;
+ll gcd(ll a,ll b,ll&x,ll&y){
+    if(!b){x=1;y=0;return a;}
     ll x1,y1;
     ll d=gcd(b,a%b,x1,y1);
-    x=y1;
-    y=x1-y1*(a/b);
+    x=y1;y=x1-y1*(a/b);
     return d;
-}
-ll modInv(ll a,ll m){ // Modular Inverse
-    ll x,y,g=gcd(a,m,x,y);
+}ll modInv(ll a,ll m){
+    ll x,y;
+    ll g=gcd(a,m,x,y);
     if(g!=1)return-1;
     return(x%m+m)%m;
-}
-ll C(ll n,ll r,ll m){ // Combination
-    if(r<0||r>n)return 0;
-    if(r==0||r==n)return 1;
-    return precomputedC[m][n][r];
-}
-ll POW(ll x,ll n,ll m){ // x^n mod m
-    if(n==0)return 1;
-    ll u=POW(x,n/2,m);
-    u=(u*u)%m;
-    if(n&1)u=(u*x)%m;
-    return u;
-}
-ll VpNfact(ll n,ll p){ // Vp(n!)
+}ll VpNfact(ll n,ll p){
     ll s=0;
-    while(n){
-        n/=p;
-        s+=n;
-    }
+    while(n){n/=p;s+=n;}
     return s;
-}
-ll U(ll n,ll p,ll pq){ // n!p
-    if(!n)return 1;
-    if(preprodCache.find(pq)==preprodCache.end()){
-        vector<ll>preprod(pq);
-        preprod[0]=1;
-        for(int i=1;i<pq;i++){
-            if(i%p==0)preprod[i]=preprod[i-1];
-            else preprod[i]=(preprod[i-1]*i)%pq;
-        }
-        preprodCache[pq]=preprod;
+}ll U27(ll n){
+    if(n==0)return 1;
+    ll res=1;
+    while(n){
+        ll exp=n/27;
+        ll exp_red=exp%18;
+        ll factor=pow27_base[exp_red];
+        res=(res*factor)%27;
+        res=(res*preprod27[n%27])%27;
+        n/=3;
     }
-    vector<ll>&preprod=preprodCache[pq];
-    ll r=POW(preprod[pq-1],n/pq,pq);
-    r=(r*preprod[n%pq])%pq;
-    return(r*U(n/p,p,pq))%pq;
-}
-ll LT(ll n,ll k,ll m){ // Lucas's Theorem
-    ll r=1;
-    while(k){
-        ll ni=n%m,ki=k%m;
-        if(ki>ni)return 0;
-        r=(r*C(ni,ki,m))%m;
-        n/=m;
-        k/=m;
-    }
-    return r;
-}
-ll LTE(ll n,ll k,ll p,ll q){ // Lucas's Theorem Expanded
+    return res;
+}ll C27(ll n,ll k){
     if(k<0||k>n)return 0;
     if(k==0||k==n)return 1;
-    ll pq=1;
-    for(int i=0;i<q;i++)pq*=p;
-    ll v=VpNfact(n,p)-VpNfact(k,p)-VpNfact(n-k,p);
-    if(v>=q)return 0;
-    ll numerator=U(n,p,pq);
-    ll denominator=(U(k,p,pq)*U(n-k,p,pq))%pq;
-    ll invDen=modInv(denominator,pq);
-    if(invDen==-1)return 0;
-    ll r=numerator*invDen%pq;
-    ll pow_p=1;
-    for(int i=0;i<v;i++)pow_p=(pow_p*p)%pq;
-    r=(r*pow_p)%pq;
-    return r;
-}
-ll CRT(ll n,ll k){ // Chinese Remainder Theorem
-    if(k+k>n)k=n-k;
+    ll v=VpNfact(n,3)-VpNfact(k,3)-VpNfact(n-k,3);
+    if(v>=3)return 0;
+    ll num=U27(n);
+    ll den=(U27(k)*U27(n-k))%27;
+    ll inv_den=modInv(den,27);
+    if(inv_den==-1)return 0;
+    ll res=num*inv_den%27;
+    if(v==1)res=res*3%27;
+    else if(v==2)res=res*9%27;
+    return res;
+}ll LT(ll n,ll k,ll m){
+    if(k<0||k>n)return 0;
+    vector<vector<ll>>&table=preC[m];
+    ll res=1;
+    while(n||k){
+        ll ni=n%m,ki=k%m;
+        if(ki>ni)return 0;
+        res=(res*table[ni][ki])%m;
+        n/=m;k/=m;
+    }
+    return res;
+}ll CRT(ll n,ll k){
+    if(k<0||k>n)return 0;
+    if(k>n-k)k=n-k;
     ll r[4];
+    r[0]=C27(n,k);
+    r[1]=LT(n,k,11);
+    r[2]=LT(n,k,13);
+    r[3]=LT(n,k,37);
+    ll res=0;
     for(int i=0;i<4;i++){
-        if(M[i]==27)r[i]=LTE(n,k,3,3);
-        else r[i]=LT(n,k,M[i]);
+        ll term=r[i]*N_list[i]%MOD;
+        term=term*crt_inv[i]%MOD;
+        res=(res+term)%MOD;
     }
-    ll result=0;
-    for(int i=0;i<4;i++){
-        ll temp=(r[i]*N[i])%MOD;
-        temp=(temp*modInv(N[i],M[i]))%MOD;
-        result=(result+temp)%MOD;
-    }
-    return result;
-}
-void precompute(){
-    ll mods[]={27,11,13,37};
+    return(res%MOD+MOD)%MOD;
+}void pre(){
+    vector<ll>mods={11,13,37};
     for(ll m:mods){
         vector<vector<ll>>C_table(50,vector<ll>(50,0));
         for(int i=0;i<50;i++){
-            C_table[i][0]=C_table[i][i]=1;
-            for(int j=1;j<i;j++){
+            C_table[i][0]=1;
+            if(i>0)C_table[i][i]=1;
+            for(int j=1;j<i;j++)
                 C_table[i][j]=(C_table[i-1][j-1]+C_table[i-1][j])%m;
-            }
         }
-        precomputedC[m]=C_table;
+        preC[m]=C_table;
     }
-}
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    precompute();
-    int T;cin>>T;
+    preprod27.resize(27);
+    preprod27[0]=1;
+    for(int i=1;i<27;i++){
+        if(i%3==0)preprod27[i]=preprod27[i-1];
+        else preprod27[i]=(preprod27[i-1]*i)%27;
+    }
+    ll base27=preprod27[26];
+    pow27_base.resize(18);
+    pow27_base[0]=1;
+    for(int i=1;i<18;i++)pow27_base[i]=(pow27_base[i-1]*base27)%27;
+    for(int i=0;i<4;i++){
+        N_list[i]=MOD/M_list[i];
+        crt_inv[i]=modInv(N_list[i],M_list[i]);
+    }
+}int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    pre();int T;cin>>T;
     while(T--){
         ll n,k;cin>>n>>k;
         cout<<CRT(n,k)<<'\n';
     }
-    return 0;
 }
