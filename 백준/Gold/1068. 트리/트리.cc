de1691dcb668d9840a9c9477c0f7235f6609f225
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-11-29 19:37:50 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<bitset>
-#include<vector>
 using namespace std;
-short N;vector<short>A;bitset<51>D,L;
+short N,A[51];bitset<51>D,L;
 void k(short a){
     D[a]=0;L[a]=0;for(short i=0;i<N;i++)
         if(A[i]==a)k(i);
 }int main(){
-    scanf("%d",&N);
-    A=vector<short>(N);for(short i=0;i<N;i++)scanf("%d",&A[i]);
+    scanf("%d",&N);for(short i=0;i<N;i++)scanf("%d",&A[i]);
     D.set();L.set();
     int I;scanf("%d",&I);k(I);
     for(short i=0;i<N;i++)
