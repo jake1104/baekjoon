6b2916920d84ccc9b6a9784a12997a34896953f6
jake1104 <81201317+jake1104@users.noreply.github.com>
2025-11-29 19:31:41 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,17 +1,17 @@
 #include<iostream>
+#include<bitset>
 #include<vector>
 using namespace std;
-int N;vector<int>A;vector<bool>D,L;
-void k(int a){
-    D[a]=0;L[a]=0;for(int i=0;i<N;i++)
+short N;vector<short>A;bitset<51>D,L;
+void k(short a){
+    D[a]=0;L[a]=0;for(short i=0;i<N;i++)
         if(A[i]==a)k(i);
 }int main(){
     cin>>N;
-    A=vector<int>(N);for(int i=0;i<N;i++)cin>>A[i];
-    D=vector<bool>(N,1),L=vector<bool>(N,1);
+    A=vector<short>(N);for(short i=0;i<N;i++)cin>>A[i];
+    D.set();L.set();
     int I;cin>>I;k(I);
-    for(int i=0;i<N;i++)
+    for(short i=0;i<N;i++)
         if(A[i]!=-1&&D[i])L[A[i]]=0;
-    int s=0;for(int i:L)s+=i;
-    cout<<s;
+    cout<<L.count()-51+N;
 }
