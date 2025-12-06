fab5dccb2d6470815a9b73f27d8dc797ad184008
jake1104 <iamjake1104@gmail.com>
2025-12-06 22:10:17 +0900

    [Bronze III] Title: 서울사이버대학을 다니고, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/30958. 서울사이버대학을 다니고/서울사이버대학을 다니고.cc
+++ 백준/Bronze/30958. 서울사이버대학을 다니고/서울사이버대학을 다니고.cc
@@ -2,15 +2,15 @@
 using namespace std;
 typedef long long ll;
 int main(){
-    ios::sync_with_stdio(false);cin.tie(nullptr);
-    string S="my era of success began with seoul cyber university, since attending seoul cyber university, more companies have been seeking me out, my life has changed since i went to seoul cyber university, the strategy that changes the future, seoul cyber university. ";
-    ll N;cin>>N;string d;getline(cin,d);
-    ll L=S.size();
-    ll f=N/L,r=N%L;
-    ll C[26]={},a=0;
-    for(char c:S)if(c>96&&c<123)C[c-97]++;
-    for(int i=0;i<26;i++)C[i]*=f;
-    for(int i=0;i<r;i++){char c=S[i];if(c>96&&c<123)C[c-97]++;}
-    for(ll i:C)a=max(a,i);
-    cout<<a;
+  ios::sync_with_stdio(0);cin.tie(0);
+  string S="my era of success began with seoul cyber university, since attending seoul cyber university, more companies have been seeking me out, my life has changed since i went to seoul cyber university, the strategy that changes the future, seoul cyber university. ";
+  ll N;cin>>N;string d;getline(cin,d);
+  ll L=S.size(),f=N/L,r=N%L,ans=0;
+  for(char x='a';x<='z';x++){
+    ll k=0;
+    for(int i=0;i<L;i++) if(S[i]==x) k++;
+    ll c=k*f;
+    for(int i=0;i<r;i++) if(S[i]==x) c++;
+    ans=max(ans,c);
+  }cout<<ans;
 }
