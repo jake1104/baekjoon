f5400e16ea36f2d71a6aadc6a1d690f6698a6f7b
jake1104 <iamjake1104@gmail.com>
2025-12-21 13:49:36 +0900

    [Silver V] Title: 귀찮음, Time: 108 ms, Memory: 9612 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.cc
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
-int n,*a,*s;
-int f(int l,int r){
+typedef long long ll;
+ll n,*a,*s;
+ll f(int l,int r){
     if(l==r)return 0;
     if(a[l]<a[r])return a[l]*(s[r]-s[l])+f(l+1,r);
     return a[r]*(s[r-1]-s[l]+a[l])+f(l,r-1);
 }int main(){
     int n;cin>>n;
-    a=new int[n],s=new int[n];
+    a=new ll[n],s=new ll[n];
     for(int i=0;i<n;i++)cin>>a[i];
     s[0]=a[0];for(int i=1;i<n;i++)s[i]=a[i]+s[i-1];
     cout<<f(0,n-1);
