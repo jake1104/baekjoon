6977ec3f3e972150bc704ef6e46da1424e6a34cf
jake1104 <iamjake1104@gmail.com>
2026-02-07 11:04:32 +0900

    [Platinum IV] Title: 외심과 내심은 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/16480. 외심과 내심은 사랑입니다/외심과 내심은 사랑입니다.cc
+++ 백준/Platinum/16480. 외심과 내심은 사랑입니다/외심과 내심은 사랑입니다.cc
@@ -1,6 +1,2 @@
 #include<iostream>
-using namespace std;
-int main(){
-    long long R,r;cin>>R>>r;
-    cout<<R*R-2*R*r;
-}
+using namespace std;int main(){long long R,r;cin>>R>>r;cout<<R*(R-2*r);}
