ac6e0a832c24929c89547bdf89a10a57579588f1
jake1104 <iamjake1104@gmail.com>
2026-04-04 16:12:37 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;cin>>T;while(T--){
+        int A,B;cin>>A>>B;
+        if(B&&!A)cout<<"no\n";
+        else cout<<"yes\n";
+    }
+}
