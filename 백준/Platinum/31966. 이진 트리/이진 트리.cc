0eeecee97b429e4c16e39c4e14977d3cfac1add0
jake1104 <iamjake1104@gmail.com>
2026-04-12 10:16:02 +0900

    [Platinum IV] Title: 이진 트리, Time: 8 ms, Memory: 2024 KB, Score: 15 point -BaekjoonHub

--- 백준/Platinum/31966. 이진 트리/이진 트리.cc
+++ 백준/Platinum/31966. 이진 트리/이진 트리.cc
@@ -1,28 +1,29 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int N;
-vector<int>A,B,lc,Ss;
-vector<int>frs,fls;
-int fr(int T){
+typedef long long ll;
+ll N;
+vector<ll>A,B,lc,Ss;
+vector<ll>frs,fls;
+int fr(ll T){
     if(frs[T])return frs[T];
     return frs[T]=fr(A[T])+fr(B[T])+lc[B[T]]-1;
-}int fl(int T){
+}int fl(ll T){
     if(fls[T])return fls[T];
     return fls[T]=fl(B[T])+fl(A[T])+lc[A[T]]-1;
-}int S(int T){
+}int S(ll T){
     if(Ss[T])return Ss[T];
-    int At=A[T],Bt=B[T];
+    ll At=A[T],Bt=B[T];
     return Ss[T]=S(At)+S(Bt)+fl(At)*lc[Bt]+lc[At]*fr(Bt)-1;
 }int main(){
     cin>>N;
-    A=vector<int>(N+1),B=vector<int>(N+1);
-    lc=vector<int>(N+1,1);
-    frs=vector<int>(N+1,0);
-    fls=vector<int>(N+1,0);
-    Ss=vector<int>(N+1,0);
+    A=vector<ll>(N+1),B=vector<ll>(N+1);
+    lc=vector<ll>(N+1,1);
+    frs=vector<ll>(N+1,0);
+    fls=vector<ll>(N+1,0);
+    Ss=vector<ll>(N+1,0);
     frs[0]=1,fls[0]=1,Ss[0]=1;
-    for(int i=1;i<=N;i++){
+    for(ll i=1;i<=N;i++){
         cin>>A[i]>>B[i];
         lc[i]=lc[A[i]]+lc[B[i]];
         cout<<S(i)<<'\n';
