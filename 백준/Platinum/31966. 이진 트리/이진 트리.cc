f8ddb6352278b0724d40478202d08b7c45eed2e8
jake1104 <iamjake1104@gmail.com>
2026-04-12 11:03:21 +0900

    [Platinum IV] Title: 이진 트리, Time: 40 ms, Memory: 6728 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/31966. 이진 트리/이진 트리.cc
+++ 백준/Platinum/31966. 이진 트리/이진 트리.cc
@@ -1,29 +1,31 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-const int MOD=1000000007;
-int N;
-vector<int>A,B,lc,Ss;
-vector<int>frs,fls;
-int fr(int T){
+typedef long long ll;
+const ll MOD=1000000007;
+ll N;
+vector<ll>A,B,lc,Ss,frs,fls;
+ll fr(ll T){
     if(frs[T])return frs[T];
     return frs[T]=(fr(A[T])+fr(B[T])+lc[B[T]]-1)%MOD;
-}int fl(int T){
+}ll fl(ll T){
     if(fls[T])return fls[T];
     return fls[T]=(fl(B[T])+fl(A[T])+lc[A[T]]-1)%MOD;
-}int S(int T){
+}ll S(ll T){
     if(Ss[T])return Ss[T];
-    int At=A[T],Bt=B[T];
+    ll At=A[T],Bt=B[T];
     return Ss[T]=(S(At)+S(Bt)+fl(At)*lc[Bt]+lc[At]*fr(Bt)-1)%MOD;
 }int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);cout.tie(nullptr);
     cin>>N;
-    A=vector<int>(N+1),B=vector<int>(N+1);
-    lc=vector<int>(N+1,1);
-    frs=vector<int>(N+1,0);
-    fls=vector<int>(N+1,0);
-    Ss=vector<int>(N+1,0);
+    A=vector<ll>(N+1),B=vector<ll>(N+1);
+    lc=vector<ll>(N+1,1);
+    frs=vector<ll>(N+1,0);
+    fls=vector<ll>(N+1,0);
+    Ss=vector<ll>(N+1,0);
     frs[0]=1,fls[0]=1,Ss[0]=1;
-    for(int i=1;i<=N;i++){
+    for(ll i=1;i<=N;i++){
         cin>>A[i]>>B[i];
         lc[i]=(lc[A[i]]+lc[B[i]])%MOD;
         cout<<S(i)<<'\n';
